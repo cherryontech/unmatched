898e2b9ecea828c123710ea70629d53cf4316dc3
halimAremu <halimat3000@gmail.com>
2023-10-22 13:51:28 -0500

    added one card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,20 @@
-import Resh from '../assets/reshma.jpg'
+import Resh from '../assets/reshma.jpg';
+// import data from '../data/data.json'
 
-const Card= () => {
+const Card = () => {
   return (
     <div className="w-full py-[10rem] px-4 bg-white">
-    <div className="max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8 ">
-      <div className="w-full border">
-        <img src={Resh} className="max-w-xs mx-auto bg-white" alt="woman"/>
-        <h2 className='text-2xl font-bold text-center py-8'>Resh Sar</h2>
-
+      <div className="max-w-[1040px] grid md:grid-cols-3 gap-8 ">
+        <div className="relative h-96 w-80 border-2 shadow-xl flex flex-col p-6 my-2 rounded-[13px] hover:scale-105 duration-300">
+          <img
+            className="object-cover h-80 bg-white rounded-[13px]"
+            src={Resh}
+            alt="woman"
+          />
+          <h2 className="text-2xl font-bold text-center py-8">Reshma Saujani</h2>
+        </div>
       </div>
     </div>
-
-    </div>
-  )
-}
+  );
+};
 export default Card;
