ba1749d45296bc2c4045518782de2695a844d606
halimAremu <halimat3000@gmail.com>
2023-10-22 16:53:51 -0500

    cards layout

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,9 @@
 import Resh from '../assets/reshma.jpg';
 // import data from '../data/data.json'
 
-const Card = () => {
+const Card = (props) => {
   return (
+    <>
     <div className="w-full py-[10rem] px-4 bg-white">
       <div className="max-w-[1040px] grid md:grid-cols-3 gap-8 ">
         <div className="relative h-96 w-80 border-2 shadow-xl flex flex-col p-6 my-2 rounded-[13px] hover:scale-105 duration-300">
@@ -10,11 +11,52 @@ const Card = () => {
             className="object-cover h-80 bg-white rounded-[13px]"
             src={Resh}
             alt="woman"
-          />
-          <h2 className="text-2xl font-bold text-center py-8">Reshma Saujani</h2>
+            />
+          <h2 className="text-2xl font-bold text-center py-8">{props.name}</h2>
+        </div>
+        <div className="relative h-96 w-80 border-2 shadow-xl flex flex-col p-6 my-2 rounded-[13px] hover:scale-105 duration-300">
+          <img
+            className="object-cover h-80 bg-white rounded-[13px]"
+            src={Resh}
+            alt="woman"
+            />
+          <h2 className="text-2xl font-bold text-center py-8">{props.name}</h2>
+        </div>
+        <div className="relative h-96 w-80 border-2 shadow-xl flex flex-col p-6 my-2 rounded-[13px] hover:scale-105 duration-300">
+          <img
+            className="object-cover h-80 bg-white rounded-[13px]"
+            src={Resh}
+            alt="woman"
+            />
+          <h2 className="text-2xl font-bold text-center py-8">{props.name}</h2>
+        </div>
+        <div className="relative h-96 w-80 border-2 shadow-xl flex flex-col p-6 my-2 rounded-[13px] hover:scale-105 duration-300">
+          <img
+            className="object-cover h-80 bg-white rounded-[13px]"
+            src={Resh}
+            alt="woman"
+            />
+          <h2 className="text-2xl font-bold text-center py-8">{props.name}</h2>
+        </div>
+        <div className="relative h-96 w-80 border-2 shadow-xl flex flex-col p-6 my-2 rounded-[13px] hover:scale-105 duration-300">
+          <img
+            className="object-cover h-80 bg-white rounded-[13px]"
+            src={Resh}
+            alt="woman"
+            />
+          <h2 className="text-2xl font-bold text-center py-8">{props.name}</h2>
+        </div>
+        <div className="relative h-96 w-80 border-2 shadow-xl flex flex-col p-6 my-2 rounded-[13px] hover:scale-105 duration-300">
+          <img
+            className="object-cover h-80 bg-white rounded-[13px]"
+            src={Resh}
+            alt="woman"
+            />
+          <h2 className="text-2xl font-bold text-center py-8">{props.name}</h2>
         </div>
       </div>
     </div>
+   </>
   );
 };
 export default Card;
