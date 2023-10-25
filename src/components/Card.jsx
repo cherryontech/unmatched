b7aaa1bea5d49ab75afa52f4d397e68b50168ff4
halimAremu <halimat3000@gmail.com>
2023-10-25 16:39:47 -0500

    refactors data

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,47 +1,52 @@
 import Resh from '../assets/reshma.jpg';
 import {useState} from 'react'
 import SingleCard from './SingleCard';
-const data = 
-[
-    {
-        "id": 1,
-        "name":"Reshma Saujani",
-        "body":"Asian founder of Girls Who Code",
-        "image": Resh
+import {data} from "../assets/BioData.js"
+// const data = 
+// [
+//     {
+//         "id": 1,
+//         "name":"Reshma Saujani",
+//         "body":"Asian founder of Girls Who Code",
+//         "image": Resh,
+//         "tag":[]
 
         
-    },
-    {
-        "id": 2,
-        "name":"Anna Lytical",
-        "body":"LGBTQ+ coding educator using drag persona",
-        "image": Resh
-    },
-    {
-        "id": 3,
-        "name":"Farida Bedwei",
-        "body":"A Ghanaian engineer with cerebral palsy",
-        "image": Resh
-    },
-    {
-        "id": 4,
-        "name":"Jedidah Isler",
-        "body":"African American astrophysicist and professor",
-        "image": Resh
-        }
-]
+//     },
+//     {
+//         "id": 2,
+//         "name":"Anna Lytical",
+//         "body":"LGBTQ+ coding educator using drag persona",
+//         "image": Resh,
+//         "tag":[]
+//     },
+//     {
+//         "id": 3,
+//         "name":"Farida Bedwei",
+//         "body":"A Ghanaian engineer with cerebral palsy",
+//         "image": Resh,
+//         "tag":[]
+//     },
+//     {
+//         "id": 4,
+//         "name":"Jedidah Isler",
+//         "body":"African American astrophysicist and professor",
+//         "image": Resh,
+//         "tag":[]
+//         }
+// ]
 
 const Card = (props) => {
   const[profile, setProfile] = useState(data)
   const details = profile.map((detail)=>{
-    return(<SingleCard image={detail.image} name = {detail.name} body = {detail.body}/>)
+    return(<SingleCard id = {detail.id} key = {detail.id} image={detail.image} name = {detail.name} body = {detail.body}/>)
   }
 
   )
   return (
     <>
     <div className="w-full py-[10rem] px-4 bg-white">
-      <div className="max-w-[1040px] grid md:grid-cols-3 gap-8 ">
+      <div className="max-w-[1040px] grid md:grid-cols-3 gap-12 ">
         {details}
       </div>
     </div>
