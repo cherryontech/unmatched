88bbc9e8749f17842d6595dd7e9b183dc620251e
halimAremu <halimat3000@gmail.com>
2023-10-25 17:20:50 -0500

    merge conflict resolution

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,36 +1,7 @@
 // import Resh from '../assets/reshma.jpg';
 import { useState } from 'react'
 import SingleCard from './SingleCard';
-import { data } from '../assets/bioData.js';
-// const data =
-// [
-//     {
-//         "id": 1,
-//         "name":"Reshma Saujani",
-//         "body":"Asian founder of Girls Who Code",
-//         "image": Resh
-
-
-//     },
-//     {
-//         "id": 2,
-//         "name":"Anna Lytical",
-//         "body":"LGBTQ+ coding educator using drag persona",
-//         "image": Resh
-//     },
-//     {
-//         "id": 3,
-//         "name":"Farida Bedwei",
-//         "body":"A Ghanaian engineer with cerebral palsy",
-//         "image": Resh
-//     },
-//     {
-//         "id": 4,
-//         "name":"Jedidah Isler",
-//         "body":"African American astrophysicist and professor",
-//         "image": Resh
-//         }
-// ]
+import { data } from '../assets/BioData.js';
 
 const Card = (props) => {
   const[profile, setProfile] = useState(data)
