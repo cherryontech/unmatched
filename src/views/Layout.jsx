3c731bd14281b4cd33e496283dfb71c5d525743f
Archaa Work <archaaworks@Archaas-MacBook-Air.local>
2023-10-27 11:53:30 -0700

    feat: adds working links to navigation buttons and adds style to match design file

--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -9,18 +9,12 @@ import logo from '../assets/logo.png';
 export function Layout() {
   return (
     <div className="flex flex-col w-full h-full min-h-screen">
-      <div className="p-4 w-full border-b-4 border-accent">
+      <div className="p-4 w-full">
         <div className="flex justify-between items-center mx-auto">
-          {/* Logo */}
           <div className="flex-shrink-0">
             <img src={logo} alt="Your Logo" className="h-10" />
           </div>
-
-          {/* Navigation */}
-          <div className="space-x-4">
-            <button className="btn btn-secondary">About Us</button>
-            <button className="btn btn-secondary">Resources</button>
-          </div>
+          <NavBar />
         </div>
       </div>
       <main className="flex-1 flex flex-col w-full">
