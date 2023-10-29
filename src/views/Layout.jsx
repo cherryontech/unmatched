acd8cd55d42c7fbfbc3ee6663e2f99faf24a0cfc
halimAremu <halimat3000@gmail.com>
2023-10-29 08:42:27 -0500

    navbar

--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -3,28 +3,21 @@ import React from 'react';
 import { Outlet, Link } from 'react-router-dom';
 
 // LOCAL IMPORTS
-import NavBar from '../components/NavBar.jsx';
+import NavBar from '../components/Navbar';
 import logo from '../assets/logo.png';
+
 
 export function Layout() {
   return (
     <div className="flex flex-col w-full h-full min-h-screen">
-      <div className="p-4 w-full border-b-4 border-accent">
+      <div className="p-4 w-full">
         <div className="flex justify-between items-center mx-auto">
-          {/* Logo */}
           <div className="flex-shrink-0">
-            <img src={logo} alt="Your Logo" className="h-10" />
+            <Link to="/">
+              <img src={logo} alt="Your Logo" className="h-10" />
+            </Link>
           </div>
-
-          {/* Navigation */}
-          <div className="space-x-4">
-            <span className='font-bold pr-4'>About Us</span>
-              <Link to="/professionals">
-            <button className="btn btn-secondary p-2 rounded-lg font-bold">
-              Resources
-              </button>
-              </Link>
-          </div>
+          <NavBar/>
         </div>
       </div>
       <main className="flex-1 flex flex-col w-full">
