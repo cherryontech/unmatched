a861f20bf5a863876e81d303dc5e10a36d357cc3
halimAremu <halimat3000@gmail.com>
2023-10-31 13:47:41 -0500

    refactored tags to use map function

--- src/assets/bioData.js
+++ src/assets/bioData.js
@@ -1,24 +1,13 @@
 
 
 import Resh from './reshma.jpg';
-import Farida from './farida.jpeg';
-import Jedidah from './Jedidah.png';
-import Maggie from './Maggie.png';
-import Maya from './Maya.png';
-import Nicole from './Nicole.png';
-import Anna from  './AnnaLytical.png';
-import Mel from './Melissa.png';
-import Rana from './Rana.png';
-import Tazin from './Tazin.png';
-import James from './james.jpeg'
-import Sebra from './sabra.jpeg'
 export const data =[
     {
         "id": 1,
         "name":"Reshma Saujani",
         "body":"Asian founder of Girls Who Code",
         "image": Resh,
-        "tag":['asian','women']
+        "tag":[]
 
         
     },
@@ -26,79 +15,21 @@ export const data =[
         "id": 2,
         "name":"Anna Lytical",
         "body":"LGBTQ+ coding educator using drag persona",
-        "image": Anna,
-        "tag":['lgbtq+']
+        "image": Resh,
+        "tag":[]
     },
     {
         "id": 3,
         "name":"Farida Bedwei",
         "body":"A Ghanaian engineer with cerebral palsy",
-        "image": Farida,
-        "tag":['black', 'women', 'disability']
+        "image": Resh,
+        "tag":[]
     },
     {
         "id": 4,
         "name":"Jedidah Isler",
         "body":"African American astrophysicist and professor",
-        "image": Jedidah,
-        "tag":['black', 'women']
-        },
-    {
-        "id": 5,
-        "name":"Maya Ajmera",
-        "body":"Asian woman who is President of Society for Science ",
-        "image": Maya,
-        "tag":['asian', 'women']
-        },
-    {
-        "id": 6,
-        "name":"Rana Dajani",
-        "body":"Palestinian molecular biologist and professor of biology",
-        "image": Rana,
-        "tag":['asian', 'women']
-        },
-    {
-        "id": 7,
-        "name":"Nicole Aunapu Mann",
-        "body":"The first Indigenous woman in space",
-        "image": Nicole,
-        "tag":['women']
-        },
-    {
-        "id": 8,
-        "name":"Melissa Cristina Márquez",
-        "body":" Puerto Rican marine biologist and science communicator",
-        "image": Mel,
-        "tag":['latin', 'women']
-        },
-    {
-        "id": 9,
-        "name":"Maggie Aderin-Pocock",
-        "body":"Black space scientist, with dyslexia at a young age",
-        "image": Maggie,
-        "tag":['black', 'women']
-        },
-    {
-        "id": 10,
-        "name":"James Makokis",
-        "body":"A Two-Spirit doctor specializing in transgender health”",
-        "image": James,
-        "tag":['lgbtq+']
-        },
-    {
-        "id": 11,
-        "name":"Tazin Khan",
-        "body":"Bangladeshi-American who is Founder of Cyber Collective",
-        "image": Tazin,
-        "tag":['asian', 'women']
-        },
-   
-    {
-        "id": 12,
-        "name":"Subrahmanyan C.",
-        "body":"Indian-American Nobel Prize-winning physicist",
-        "image": Sebra,
-        "tag":['immigrant']
-        },
-   
+        "image": Resh,
+        "tag":[]
+        }
 ]
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,15 +3,20 @@ import { useState } from 'react'
 import SingleCard from './SingleCard';
 import { data } from "../assets/BioData.js";
 
-
+const buttons = ['LGBTQ+', 'WOMEN', 'BLACK', 'ASIAN', 'DISABILITY', 'LATIN', 'IMMIGRANT'];
 
 const Card = (props) => {
-  const[profile, setProfile] = useState(data)
+  const[profile, setProfile] = useState(data);
+  const [clicked, setClicked] = useState(false);
+  const buttonColor = clicked? 'bg-[#0A2E50]': 'bg-white-100 ';
+  const textColor = clicked?  'text-white-100':'text-[#0A2E50]';
+
+
   const details = profile.map((detail)=>{
     return(<SingleCard id = {detail.id} key = {detail.id} image={detail.image} name = {detail.name} body = {detail.body}/>)
   }
-
   )
+
   return (
     <>
     <h1 className='text-[30px] font-bold text-center mt-12 text-[#0A2E50]'>You are in good company</h1>
@@ -32,14 +37,14 @@ const Card = (props) => {
   
 </form>
 <div className='flex items-center justify-center mt-4 m-auto'>
+{
+  buttons.map((button)=>{
+    return(
+      <button  className="bg-white-100 text-[#0A2E50] text-xs font-medium mr-2 px-2.5 py-0.5  rounded-[20px] border border-[#0A2E50] p-3">{button}</button>
+    )
+  })
+};
 
-<button  className="bg-white-100 text-[#0A2E50] text-xs font-medium mr-2 px-2.5 py-0.5 rounded-[20px] dark:bg-gray-700 dark:text-blue-400 border border-[#0A2E50] p-3">LGBTQ+</button>
-<button  className="bg-white-100 text-[#0A2E50] text-xs font-medium mr-2 px-2.5 py-0.5  rounded-[20px] dark:bg-gray-700 dark:text-gray-400 border border-[#0A2E50] p-3">WOMEN</button>
-<button  className="bg-white-100 text-[#0A2E50] text-xs font-medium mr-2 px-2.5 py-0.5  rounded-[20px] dark:bg-gray-700 dark:text-red-400 border border-[#0A2E50] p-3">BLACK</button>
-<button  className="bg-white-100 text-[#0A2E50] text-xs font-medium mr-2 px-2.5 py-0.5  rounded-[20px] dark:bg-gray-700 dark:text-green-400 border border-[#0A2E50] p-3">ASIAN</button>
-<button  className="bg-yewhite-100 text-[#0A2E50] text-xs font-medium mr-2 px-2.5 py-0.5  rounded-[20px] dark:bg-gray-700 dark:text-yellow-300 border border-[#0A2E50] p-3">DISABILITY</button>
-<button  className="bg-white-100 text-[#0A2E50] text-xs font-medium mr-2 px-2.5 py-0.5  rounded-[20px] dark:bg-gray-700 dark:text-purple-400 border border-[#0A2E50] p-3">LATIN</button>
-<button  className="bg-white-100 text-[#0A2E50] text-xs font-medium mr-2 px-2.5 py-0.5  rounded-[20px] dark:bg-gray-700 dark:text-pink-400 border border-[#0A2E50] p-3">IMMIGRANT</button>
 
 </div>
     <div className="w-full py-[10rem] px-4 bg-white ">
