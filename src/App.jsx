fc5154b5e990c457d17f1d7569d1c52dda2036ff
halimAremu <halimat3000@gmail.com>
2023-11-02 16:26:31 -0500

    merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // LIBRARY IMPORTS
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import {
 	BrowserRouter as Router,
 	Routes,
@@ -7,14 +7,19 @@ import {
 } from 'react-router-dom';
 
 // LOCAL IMPORTS
-
+import { bioData } from './assets/bioData.js';
 import { Home, Layout, ProfessionalsPage } from './views';
 
 function App() {
+  // SET STATES
+  const [data, setData] = useState(bioData);
+
+  // SET REFERENCES
+  const fullDataSetRef = useRef(bioData);
 
   return (
     <Router>
-     
+
       <Routes>
         <Route
           path="/"
@@ -28,7 +33,7 @@ function App() {
 					/>
           <Route
 						path="/professionals"
-						element={<ProfessionalsPage />}
+						element={<ProfessionalsPage data={data} setData={setData} fullDataSet={fullDataSetRef.current} />}
 					/>
         </Route>
       </Routes>
