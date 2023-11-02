30cfb23a8bb48482a8be6758687e506bb0ad7537
Archaa Work <archaaworks@Archaas-MacBook-Air.local>
2023-11-02 14:51:26 -0700

    need to deal with bioData vs BioData files

--- src/assets/bioData.js
+++ src/assets/bioData.js
@@ -1,171 +1,35 @@
-import Reshma from './images/Reshma.jpg';
-import Anna from './images/Anna.jpg';
-import Farida from './images/Farida.jpg';
-import Jedidah from './images/Jedidah.png';
-import Maya from './images/Maya.png';
-import Rana from './images/Rana.avif';
-import Nicole from './images/Nicole.jpg';
-import Melissa from './images/Melissa.png';
-import Maggie from './images/Maggie.jpg';
-import James from './images/James.jpg';
-import Tazin from './images/Tazin.jpg';
-import Subrah from './images/Subrahmanyan.jpg';
 
-export const bioData = [
-  {
-    id: 1,
-    name: 'Reshma',
-    fullName: 'Reshma Saujani',
-    occupation: 'Founder of Girls Who Code',
-    bio: {
-      summary: `Reshma Saujani is a renowned activist and author committed to empowering women and girls, bridging the tech gender gap, and advocating for moms. She is best known as the founder of Girls Who Code, a non-profit organization closing the tech gender gap by offering girls, women, and nonbinary people access to programming education. Reshma began her career as an attorney and made history as the first Indian American woman to run for U.S. Congress in 2010. Witnessing the gender gap in computer science classrooms during her campaign inspired her to create Girls Who Code in 2012.`,
-      career: `In her 2023 Smith College commencement speech, Reshma shared about her upbringing as the child of Indian refugees living in the U.S. She recalled feeling isolated because no one else looked like her; she had no one to identify with. This feeling remained with her even in her successful career as an attorney. Reshma said imposter syndrome makes us look like the problem, but we’re not. She is committed to female empowerment because she said we have a broken system that makes us feel we’re not enough when we are. So it’s up to us to fix the system.`,
-      impact: `Reshma's impact extends from her bestseller Brave, Not Perfect to her influential TED talk, “Teach girls bravery, not perfection”. Girls Who Code has reached over 500 million people worldwide and has taught more than 300,000 girls how to code. Reshma’s passion for moms established Moms First in 2020, a non-profit supporting working moms through policy change.`,
+
+import Resh from './reshma.jpg';
+export const data =[
+    {
+        "id": 1,
+        "name":"Reshma Saujani",
+        "body":"Asian founder of Girls Who Code",
+        "image": Resh,
+        "tag":[]
+
+        
     },
-    image: Reshma,
-    tags: [],
-  },
-  {
-    id: 2,
-    name: 'Anna',
-    fullName: 'Anna Lytical',
-    occupation: 'LGBTQ+ Coding Educator',
-    bio: {
-      summary: ``,
-      career: ``,
-      impact: ``,
+    {
+        "id": 2,
+        "name":"Anna Lytical",
+        "body":"LGBTQ+ coding educator using drag persona",
+        "image": Resh,
+        "tag":[]
     },
-    image: Anna,
-    tags: [],
-  },
-  {
-    id: 3,
-    name: 'Farida',
-    fullName: 'Farida Bedwei',
-    occupation: 'Co-Founder of Logiciel',
-    bio: {
-      summary: ``,
-      career: ``,
-      impact: ``,
+    {
+        "id": 3,
+        "name":"Farida Bedwei",
+        "body":"A Ghanaian engineer with cerebral palsy",
+        "image": Resh,
+        "tag":[]
     },
-    image: Farida,
-    tags: [],
-  },
-  {
-    id: 4,
-    name: 'Jedidah',
-    fullName: 'Jedidah Isler',
-    occupation: 'Astrophysicist and Professor',
-    bio: {
-      summary: ``,
-      career: ``,
-      impact: ``,
-    },
-    image: Jedidah,
-    tags: [],
-  },
-  {
-    id: 5,
-    name: 'Maya',
-    fullName: 'Maya Ajmera',
-    occupation: 'CEO of Society for Science',
-    bio: {
-      summary: ``,
-      career: ``,
-      impact: ``,
-    },
-    image: Maya,
-    tags: [],
-  },
-  {
-    id: 6,
-    name: 'Rana',
-    fullName: 'Rana Dajani',
-    occupation: 'Molecular Biologist and Professor',
-    bio: {
-      summary: ``,
-      career: ``,
-      impact: ``,
-    },
-    image: Rana,
-    tags: [],
-  },
-  {
-    id: 7,
-    name: 'Nicole',
-    fullName: 'Nicole Aunapu Mann',
-    occupation: 'NASA Astronaut',
-    bio: {
-      summary: ``,
-      career: ``,
-      impact: ``,
-    },
-    image: Nicole,
-    tags: [],
-  },
-  {
-    id: 8,
-    name: 'Melissa',
-    fullName: 'Melissa Cristina Márquez',
-    occupation: ' Marine Biologist',
-    bio: {
-      summary: ``,
-      career: ``,
-      impact: ``,
-    },
-    image: Melissa,
-    tags: [],
-  },
-  {
-    id: 9,
-    name: 'Maggie',
-    fullName: 'Maggie Aderin-Pocock',
-    occupation: 'Space Scientist and Author',
-    bio: {
-      summary: ``,
-      career: ``,
-      impact: ``,
-    },
-    image: Maggie,
-    tags: [],
-  },
-  {
-    id: 10,
-    name: 'James',
-    fullName: 'James Makokis',
-    occupation: 'Two-Spirit Family Physician',
-    bio: {
-      summary: ``,
-      career: ``,
-      impact: ``,
-    },
-    image: James,
-    tags: [],
-  },
-  {
-    id: 11,
-    name: 'Tazin',
-    fullName: 'Tazin Khan',
-    occupation: 'Founder of Cyber Collective',
-    bio: {
-      summary: ``,
-      career: ``,
-      impact: ``,
-    },
-    image: Tazin,
-    tags: [],
-  },
-  {
-    id: 12,
-    name: 'Subrahmanyan',
-    fullName: 'Subrahmanyan C.',
-    occupation: 'Nobel Prize-Winning Physicist',
-    bio: {
-      summary: ``,
-      career: ``,
-      impact: ``,
-    },
-    image: Subrah,
-    tags: [],
-  },
-];
+    {
+        "id": 4,
+        "name":"Jedidah Isler",
+        "body":"African American astrophysicist and professor",
+        "image": Resh,
+        "tag":[]
+        }
+]
