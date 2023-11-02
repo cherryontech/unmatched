875490c53ac10a5adaab7c0d2db69a93dcc92bb1
Archaa Work <archaaworks@Archaas-MacBook-Air.local>
2023-11-01 21:06:30 -0700

    feat: conditionally renders successful search result message showing what user typed in search along with results

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,17 +1,20 @@
 // LIBRARY IMPORTS
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { MagnifyingGlassIcon, XMarkIcon } from '@heroicons/react/24/solid';
 
 // LOCAL IMPORTS
 
 export default function SearchBar({ data, setData, setClearedSearch, fullDataSet }) {
   // SET STATES
-  const [searchTerm, setSearchTerm] = useState('');
   const [searchSubmitted, setSearchSubmitted] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchSuccessful, setSearchSuccessful] = useState(false);
+  const searchTermRef = useRef('');
 
   // EVENT HANDLERS
   const handleInputChange = (event) => {
     setSearchTerm(event.target.value);
+    searchTermRef.current = event.target.value;
   };
   const handleInputSubmit = (event) => {
     event.preventDefault();
@@ -22,6 +25,9 @@ export default function SearchBar({ data, setData, setClearedSearch, fullDataSet
     if (results.length === 0) {
       setClearedSearch(false);
     }
+    if (results.length > 0) {
+      setSearchSuccessful(true);
+    }
     setSearchSubmitted(true);
     setSearchTerm('');
   }
@@ -31,28 +37,36 @@ export default function SearchBar({ data, setData, setClearedSearch, fullDataSet
     setSearchTerm('');
     setClearedSearch(true);
     setSearchSubmitted(false);
+    setSearchSuccessful(false);
   }
 
 
   return (
-    <form className=" w-full m-auto max-w-lg mt-2 relative" onSubmit={handleInputSubmit}>
-      <label className="label">
-        <span className="label-text-alt flex items-start text-[#0A2E50] font-bold">Who can we help you find?</span>
-      </label>
-      <div className='relative'>
-        <input
-          type="text"
-          placeholder={searchSubmitted ? "Clear your results to search again" : "Search by name"}
-          className="input input-bordered w-[1200px] p-4 rounded-xl max-w-lg flex items-center font-bold text-[#0A2E50]"
-          value={searchTerm}
-          onChange={handleInputChange}
-          disabled={searchSubmitted}
-        />
-        <button className="absolute right-1 top-1/2 -translate-y-1/2 p-4" type="submit">
-          {searchSubmitted ? <XMarkIcon className="w-5 h-5 text-primary" onClick={handleClearSubmit}/> : <MagnifyingGlassIcon className="w-5 h-5 text-primary"/>}
-        </button>
-      </div>
-    </form>
+    <>
+      <form className=" w-full m-auto max-w-lg mt-2 relative" onSubmit={handleInputSubmit}>
+        <label className="label">
+          <span className="label-text-alt flex items-start text-[#0A2E50] font-bold">Who can we help you find?</span>
+        </label>
+        <div className='relative'>
+          <input
+            type="text"
+            placeholder={searchSubmitted ? "Clear your results to search again" : "Search by name"}
+            className="input input-bordered w-[1200px] p-4 rounded-xl max-w-lg flex items-center font-bold text-[#0A2E50]"
+            value={searchTerm}
+            onChange={handleInputChange}
+            disabled={searchSubmitted}
+          />
+          <button className="absolute right-1 top-1/2 -translate-y-1/2 p-4" type="submit">
+            {searchSubmitted ? <XMarkIcon className="w-5 h-5 text-primary" onClick={handleClearSubmit}/> : <MagnifyingGlassIcon className="w-5 h-5 text-primary"/>}
+          </button>
+        </div>
+      </form>
+      {searchSuccessful && (
+        <div className="mt-4 mb-4 text-center">
+          Showing results for "{searchTermRef.current}"
+        </div>
+      )}
+    </>
   )
 };
 
--- src/views/ProfessionalsPage.jsx
+++ src/views/ProfessionalsPage.jsx
@@ -1,5 +1,5 @@
 // LIBRARY IMPORTS
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 // LOCAL IMPORTS
 import Card from '../components/Card.jsx';
@@ -13,7 +13,12 @@ export function ProfessionalsPage({ data, setData, fullDataSet }) {
   return (
     <div>
       <h1 className='text-4xl font-bold text-center mt-12 text-primary'>You're in good company</h1>
-      <SearchBar data={data} setData={setData} setClearedSearch={setClearedSearch} fullDataSet={fullDataSet} />
+      <SearchBar
+        data={data}
+        setData={setData}
+        fullDataSet={fullDataSet}
+        setClearedSearch={setClearedSearch}
+      />
       {clearedSearch ? <Card data={data} setData={setData} /> : <NoResults />}
     </div>
   )
