8288213aa668d9c81f0afb94a83895a800d21320
shyusu4 <shyusupova4@gmail.com>
2023-11-02 22:30:38 +0300

    Add BioPage route

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,22 @@
 // LIBRARY IMPORTS
 import React from 'react';
-import {
-	BrowserRouter as Router,
-	Routes,
-	Route,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // LOCAL IMPORTS
-
-import { Home, Layout, ProfessionalsPage } from './views';
+import { Home, Layout, ProfessionalsPage, BioPage } from './views';
 
 function App() {
-
   return (
     <Router>
-     
       <Routes>
-        <Route
-          path="/"
-          element={<Layout />}
-        >
-          <Route
-						index
-						element={
-							<Home />
-						}
-					/>
-          <Route
-						path="/professionals"
-						element={<ProfessionalsPage />}
-					/>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/professionals" element={<ProfessionalsPage />} />
+          <Route path="/bio/:id" element={<BioPage />} />
         </Route>
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/views/BioPage.jsx
+++ src/views/BioPage.jsx
@@ -1,8 +1,34 @@
-// LIBRARY IMPORTS
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { bioData } from '../assets/bioData.js';
 
-// LOCAL IMPORTS
+export const BioPage = () => {
+  const { id } = useParams();
+  const data = bioData.find(item => item.id === parseInt(id, 10));
 
-export function BioPage() {
-  return ()
+  return (
+    <>
+      <div>
+        <div class="h-40 overflow-hidden bg-[#0B2E50]"></div>
+        <div class="mx-auto w-44 h-44 relative -mt-16 border-8 border-white rounded-full overflow-hidden">
+          <img src={data.image} alt={data.fullName} />
+        </div>
+        <div class="text-center mt-2">
+          <h2 class="text-3xl font-bold">{data.fullName}</h2>
+          <p class="text-gray-500">{data.occupation}</p>
+        </div>
+      </div>
+
+      <section>
+        <h3 className="text-xl font-bold">Who is {data.fullName}?</h3>
+        <p>{data.bio.summary}</p>
+        <h3 className="text-xl font-bold">What is {data.name}'s impact?</h3>
+        <p>{data.bio.impact}</p>
+        <h3 className="text-xl font-bold">
+          How did {data.name} conquer imposter syndrome?
+        </h3>
+        <p>{data.bio.career}</p>
+      </section>
+    </>
+  );
 };
