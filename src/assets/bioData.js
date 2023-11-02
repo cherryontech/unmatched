f9b3e41f41d530d994f7a9f20477bbe8b0c75a83
halimAremu <halimat3000@gmail.com>
2023-11-02 15:00:22 -0500

    s

--- src/assets/bioData.js
+++ src/assets/bioData.js
@@ -1,13 +1,24 @@
 
 
 import Resh from './reshma.jpg';
+import Farida from './farida.jpeg';
+import Jedidah from './Jedidah.png';
+import Maggie from './Maggie.png';
+import Maya from './Maya.png';
+import Nicole from './Nicole.png';
+import Anna from  './AnnaLytical.png';
+import Mel from './Melissa.png';
+import Rana from './Rana.png';
+import Tazin from './Tazin.png';
+import James from './james.jpeg'
+import Sebra from './sabra.jpeg'
 export const data =[
     {
         "id": 1,
         "name":"Reshma Saujani",
         "body":"Asian founder of Girls Who Code",
         "image": Resh,
-        "tag":[]
+        "tag":['asian','women']
 
         
     },
@@ -15,21 +26,79 @@ export const data =[
         "id": 2,
         "name":"Anna Lytical",
         "body":"LGBTQ+ coding educator using drag persona",
-        "image": Resh,
-        "tag":[]
+        "image": Anna,
+        "tag":['lgbtq+']
     },
     {
         "id": 3,
         "name":"Farida Bedwei",
         "body":"A Ghanaian engineer with cerebral palsy",
-        "image": Resh,
-        "tag":[]
+        "image": Farida,
+        "tag":['black', 'women', 'disability']
     },
     {
         "id": 4,
         "name":"Jedidah Isler",
         "body":"African American astrophysicist and professor",
-        "image": Resh,
-        "tag":[]
-        }
+        "image": Jedidah,
+        "tag":['black', 'women']
+        },
+    {
+        "id": 5,
+        "name":"Maya Ajmera",
+        "body":"Asian woman who is President of Society for Science ",
+        "image": Maya,
+        "tag":['asian', 'women']
+        },
+    {
+        "id": 6,
+        "name":"Rana Dajani",
+        "body":"Palestinian molecular biologist and professor of biology",
+        "image": Rana,
+        "tag":['asian', 'women']
+        },
+    {
+        "id": 7,
+        "name":"Nicole Aunapu Mann",
+        "body":"The first Indigenous woman in space",
+        "image": Nicole,
+        "tag":['women']
+        },
+    {
+        "id": 8,
+        "name":"Melissa Cristina Márquez",
+        "body":" Puerto Rican marine biologist and science communicator",
+        "image": Mel,
+        "tag":['latin', 'women']
+        },
+    {
+        "id": 9,
+        "name":"Maggie Aderin-Pocock",
+        "body":"Black space scientist, with dyslexia at a young age",
+        "image": Maggie,
+        "tag":['black', 'women', 'disability']
+        },
+    {
+        "id": 10,
+        "name":"James Makokis",
+        "body":"A Two-Spirit doctor specializing in transgender health”",
+        "image": James,
+        "tag":['lgbtq+']
+        },
+    {
+        "id": 11,
+        "name":"Tazin Khan",
+        "body":"Bangladeshi-American who is Founder of Cyber Collective",
+        "image": Tazin,
+        "tag":['asian', 'women']
+        },
+   
+    {
+        "id": 12,
+        "name":"Subrahmanyan C.",
+        "body":"Indian-American Nobel Prize-winning physicist",
+        "image": Sebra,
+        "tag":['immigrant']
+        },
+   
 ]
