49ee3bdcc8f2e6e542be0724540c8e84b79b56d6
Havana82 <halimat3000@gmail.com>
2023-11-03 09:07:43 -0500

    Update bioData.js

--- src/assets/bioData.js
+++ src/assets/bioData.js
@@ -1,4 +1,15 @@
-
+import Reshma from './images/Reshma.jpg';
+import Anna from './images/Anna.jpg';
+import Farida from './images/Farida.jpg';
+import Jedidah from './images/Jedidah.png';
+import Maya from './images/Maya.png';
+import Rana from './images/Rana.avif';
+import Nicole from './images/Nicole.jpg';
+import Melissa from './images/Melissa.png';
+import Maggie from './images/Maggie.jpg';
+import James from './images/James.jpg';
+import Tazin from './images/Tazin.jpg';
+import Subrah from './images/Subrahmanyan.jpg';
 
 import Resh from './reshma.jpg';
 export const data =[
