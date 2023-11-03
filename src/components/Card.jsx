50a44045e51b513c0cb230845d7a3f7a98586baa
shyusu4 <shyusupova4@gmail.com>
2023-11-03 18:47:28 +0300

    Make professionals cards responsive

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,7 +6,7 @@ import SingleCard from './SingleCard';
 
 const Card = ({ data, setData }) => {
   // const[profile, setProfile] = useState(data)
-  const details = data.map((detail)=>{
+  const details = data.map(detail => {
     return (
       <SingleCard
         id={detail.id}
@@ -18,22 +18,36 @@ const Card = ({ data, setData }) => {
     );
   });
   return (
-    <>
-      <div className='flex items-center justify-center mt-4'>
-        <button  className="bg-white-100 text-[#0A2E50] text-xs font-medium mr-2 px-2.5 py-0.5 rounded-[20px] dark:bg-gray-700 dark:text-blue-400 border border-[#0A2E50] p-3">LGBTQ+</button>
-        <button  className="bg-white-100 text-[#0A2E50] text-xs font-medium mr-2 px-2.5 py-0.5  rounded-[20px] dark:bg-gray-700 dark:text-gray-400 border border-[#0A2E50] p-3">WOMEN</button>
-        <button  className="bg-white-100 text-[#0A2E50] text-xs font-medium mr-2 px-2.5 py-0.5  rounded-[20px] dark:bg-gray-700 dark:text-red-400 border border-[#0A2E50] p-3">BLACK</button>
-        <button  className="bg-white-100 text-[#0A2E50] text-xs font-medium mr-2 px-2.5 py-0.5  rounded-[20px] dark:bg-gray-700 dark:text-green-400 border border-[#0A2E50] p-3">ASIAN</button>
-        <button  className="bg-yewhite-100 text-[#0A2E50] text-xs font-medium mr-2 px-2.5 py-0.5  rounded-[20px] dark:bg-gray-700 dark:text-yellow-300 border border-[#0A2E50] p-3">DISABILITY</button>
-        <button  className="bg-white-100 text-[#0A2E50] text-xs font-medium mr-2 px-2.5 py-0.5  rounded-[20px] dark:bg-gray-700 dark:text-purple-400 border border-[#0A2E50] p-3">LATIN</button>
-        <button  className="bg-white-100 text-[#0A2E50] text-xs font-medium mr-2 px-2.5 py-0.5  rounded-[20px] dark:bg-gray-700 dark:text-pink-400 border border-[#0A2E50] p-3">IMMIGRANT</button>
+    <div className="max-w-screen-lg mx-auto">
+      <div className="flex items-center justify-center mt-4">
+        <button className="bg-white-100 text-[#0A2E50] text-xs font-medium mr-2 px-2.5 py-0.5 rounded-[20px] dark:bg-gray-700 dark:text-blue-400 border border-[#0A2E50] p-3">
+          LGBTQ+
+        </button>
+        <button className="bg-white-100 text-[#0A2E50] text-xs font-medium mr-2 px-2.5 py-0.5  rounded-[20px] dark:bg-gray-700 dark:text-gray-400 border border-[#0A2E50] p-3">
+          WOMEN
+        </button>
+        <button className="bg-white-100 text-[#0A2E50] text-xs font-medium mr-2 px-2.5 py-0.5  rounded-[20px] dark:bg-gray-700 dark:text-red-400 border border-[#0A2E50] p-3">
+          BLACK
+        </button>
+        <button className="bg-white-100 text-[#0A2E50] text-xs font-medium mr-2 px-2.5 py-0.5  rounded-[20px] dark:bg-gray-700 dark:text-green-400 border border-[#0A2E50] p-3">
+          ASIAN
+        </button>
+        <button className="bg-yewhite-100 text-[#0A2E50] text-xs font-medium mr-2 px-2.5 py-0.5  rounded-[20px] dark:bg-gray-700 dark:text-yellow-300 border border-[#0A2E50] p-3">
+          DISABILITY
+        </button>
+        <button className="bg-white-100 text-[#0A2E50] text-xs font-medium mr-2 px-2.5 py-0.5  rounded-[20px] dark:bg-gray-700 dark:text-purple-400 border border-[#0A2E50] p-3">
+          LATIN
+        </button>
+        <button className="bg-white-100 text-[#0A2E50] text-xs font-medium mr-2 px-2.5 py-0.5  rounded-[20px] dark:bg-gray-700 dark:text-pink-400 border border-[#0A2E50] p-3">
+          IMMIGRANT
+        </button>
       </div>
-      <div className="w-full py-8 px-4 bg-white ">
-        <div className="max-w-[1040px] grid md:grid-cols-3 gap-12 place-items-center m-auto  ">
+      <div className="flex flex-col items-center my-10 mx-8">
+        <div className="grid grid-cols-1 lg:gap-x-14 lg:gap-y-8 md:gap-x-10 md:gap-y-4 gap-5 sm:grid-cols-2 lg:grid-cols-3">
           {details}
         </div>
       </div>
-   </>
+    </div>
   );
 };
 export default Card;
--- src/components/ResourceCard.jsx
+++ src/components/ResourceCard.jsx
@@ -10,7 +10,7 @@ function ResourceCard({ image, title, link }) {
         alt={title}
       />
       <h2 className="text-primary font-bold text-left pt-2">{title}</h2>
-      <Link to={link}>
+      <Link to={link} target="_blank">
         <button className="bg-[#A855F7] w-[100px] rounded-[15px] text-xs font-semibold text-white mt-6 float-right mx-auto mr-0 p-1">
           Open Resource
         </button>
