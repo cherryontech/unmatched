d4a8b0b4c7ae3458bc23e1a37457bc53d13738b1
shyusu4 <shyusupova4@gmail.com>
2023-11-04 16:32:51 +0300

    Add back to resources button

--- src/views/BioPage.jsx
+++ src/views/BioPage.jsx
@@ -1,6 +1,6 @@
 // LIBRARY IMPORTS
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 // LOCAL IMPORTS
 import { bioData } from '../assets/bioData.js';
 import SingleCard from '../components/SingleCard.jsx';
@@ -38,55 +38,65 @@ export const BioPage = () => {
   };
 
   return (
-    <div className="text-primary">
-      <div className="h-52 bg-primary w-screen" />
-      <div className="mx-auto w-48 h-48 relative -mt-28 border-8 border-white rounded-full overflow-hidden">
-        <img src={data.image} alt={data.fullName} />
-      </div>
-      <div className="text-center mt-2 pb-14">
-        <h1 className="text-3xl font-bold">{data.fullName}</h1>
-        <p>{data.occupation}</p>
-      </div>
-      <div className="max-w-screen-lg xl:mx-auto md:mx-28 sm:mx-8">
-        <div className="mb-10">
-          <div className="pb-4">
-            <h3 className="text-2xl font-bold">Who is {data.fullName}?</h3>
-            {formatParagraphs(data.bio.summary)}
+    <>
+      <div className="text-primary">
+        <div className="h-52 bg-primary w-screen" />
+        <div className="mx-auto w-48 h-48 relative -mt-28 border-8 border-white rounded-full overflow-hidden">
+          <img src={data.image} alt={data.fullName} />
+        </div>
+        <div className="text-center mt-2 pb-14">
+          <h1 className="text-3xl font-bold">{data.fullName}</h1>
+          <p>{data.occupation}</p>
+        </div>
+        <div className="max-w-screen-lg xl:mx-auto md:mx-28 sm:mx-8">
+          <div className="mb-10">
+            <div className="pb-4">
+              <h3 className="text-2xl font-bold">Who is {data.fullName}?</h3>
+              {formatParagraphs(data.bio.summary)}
+            </div>
+            <div className="pb-4">
+              <h3 className="text-2xl font-bold">
+                What is {data.name}'s impact?
+              </h3>
+              {formatParagraphs(data.bio.impact)}
+            </div>
+            <div className="pb-4">
+              <h3 className="text-2xl font-bold">
+                How did {data.name} conquer imposter syndrome?
+              </h3>
+              {formatParagraphs(data.bio.career)}
+            </div>
           </div>
-          <div className="pb-4">
-            <h3 className="text-2xl font-bold">
-              What is {data.name}'s impact?
-            </h3>
-            {formatParagraphs(data.bio.impact)}
+          <div className="text-center lg:text-left">
+            <h2 className="text-2xl font-bold">
+              {data.name}'s Suggested Resources
+            </h2>
+            <div className="flex flex-col flex-wrap md:flex-row lg:justify-between justify-center items-center mb-14">
+              {data.resources.map(item => (
+                <ResourceCard
+                  image={item.image}
+                  title={item.title}
+                  link={item.link}
+                />
+              ))}
+            </div>
           </div>
-          <div className="pb-4">
-            <h3 className="text-2xl font-bold">
-              How did {data.name} conquer imposter syndrome?
-            </h3>
-            {formatParagraphs(data.bio.career)}
+          <div className="text-center lg:text-left">
+            <h2 className="text-2xl font-bold">More Unmatched Inspiration</h2>
+            <div className="flex flex-col flex-wrap md:flex-row lg:justify-between md:justify-between justify-center items-center mb-20">
+              {nextCards}
+            </div>
+          </div>
+          <div className="flex justify-end mb-7">
+            <Link
+              to="/professionals"
+              className="px-4 py-2 rounded-full font-bold bg-red-500 text-white"
+            >
+              Back to Resources
+            </Link>
           </div>
         </div>
-        <div className="text-center lg:text-left">
-          <h2 className="text-2xl font-bold">
-            {data.name}'s Suggested Resources
-          </h2>
-          <div className="flex flex-col flex-wrap md:flex-row lg:justify-between justify-center items-center mb-14">
-            {data.resources.map(item => (
-              <ResourceCard
-                image={item.image}
-                title={item.title}
-                link={item.link}
-              />
-            ))}
-          </div>
-        </div>
-        <div className="text-center lg:text-left">
-          <h2 className="text-2xl font-bold">More Unmatched Inspiration</h2>
-          <div className="flex flex-col flex-wrap md:flex-row lg:justify-between md:justify-between justify-center items-center mb-14">
-            {nextCards}
-          </div>
-        </div>
       </div>
-    </div>
+    </>
   );
 };
