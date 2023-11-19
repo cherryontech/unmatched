3cb1a29c7f99da281438140e328fa855320135e6
halimAremu <halimat3000@gmail.com>
2023-11-19 15:07:01 -0600

    Corrects style issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,9 +9,9 @@ export default function NavBar() {
   
   const [iconClick, seticonClick] = useState(clicked )
 
-  const voiceIconColor = iconClick.clickedVoiceIcon? 'bg-gradient-to-r from-red-500 to-purple-500 ' : ' bg-red-500';
-  const aboutIconColor = iconClick.clickedAboutIcon? 'border-b-4 border-b-[#0A2E50] ' : '';
-
+  const voiceIconColor = iconClick.clickedVoiceIcon? 'md:bg-gradient-to-r from-red-500 to-purple-500 ' : ' bg-red-500';
+  const aboutIconColor = iconClick.clickedAboutIcon? 'md:border-b-4 border-b-[#0A2E50] ' : '';
+  
   const changeVoicesColor = ()=>{
     const isClicked = {...clicked, clickedVoiceIcon:true}
     seticonClick(isClicked);
@@ -29,19 +29,19 @@ export default function NavBar() {
         exact
         to="/"
         onClick={ChangeAboutIcon }
-        className={`text-primary font-bold lg:${aboutIconColor} flex-col justify-start text-[2.1vh] items-center inline-flex relative group`}
+        className={`text-primary font-bold  ${aboutIconColor}  flex-col justify-start text-[2.0vh] items-center inline-flex relative group`}
       >
         <span className="text-center font-bold md:group-hover:opacity-100 ">
           About Us
         </span>
-        <span className="w-full md:h-1 h-[3px] top-5 md:top-full bg-primary rounded-2xl absolute bottom-0 md:opacity-0 transition-opacity duration-300 ease-in-out opacity-100 md:group-hover:opacity-100" />
+        <span className="w-full md:h-1 h-[3px]  top-5 md:top-full bg-primary rounded-2xl absolute bottom-0 md:opacity-0 transition-opacity duration-300 ease-in-out opacity-100 md:group-hover:opacity-100" />
       </NavLink>
       <NavLink
        to="/professionals"
        onClick={changeVoicesColor}
-        className="text-primary font-bold flex-col justify-start items-center inline-flex relative"
+        className="text-primary font-bold flex-col justify-start items-center inline-flex relative text-[2.0vh]"
       >
-        <span className={`md:px-4 md:py-1 lg:${voiceIconColor} rounded-full md:bg-red-500 md:text-white md:hover:bg-gradient-to-r from-red-500 to-purple-500 text-center font-bold`}>
+        <span className={`md:px-4 md:py-1 ${voiceIconColor}  bg-white rounded-full md:bg-red-500 md:text-white md:hover:bg-gradient-to-r from-red-500 to-purple-500 text-center font-bold`}>
           Unmatched Voices
         </span>
         <span className="w-full md:h-1 h-[3px] top-5 md:top-full bg-primary rounded-2xl absolute bottom-0 md:opacity-0 transition-opacity duration-300 ease-in-out opacity-100 md:group-hover:opacity-100" />
