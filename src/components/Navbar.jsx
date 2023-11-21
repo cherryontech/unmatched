c18a20a3db98cbc2ccbf21a90912804a50feee6b
halimAremu <halimat3000@gmail.com>
2023-11-21 05:42:31 -0600

    style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,7 +33,7 @@ export default function NavBar() {
         onClick={ChangeAboutIcon }
         className={` font-bold ${aboutIcon}  ${aboutIconColor}  flex-col justify-start text-[2.0vh] items-center inline-flex relative group`}
       >
-        <span className="text-center font-bold md:group-hover:opacity-100 md:text-primary ">
+        <span className="text-center font-bold md:group-hover:opacity-100 md:text-primary  lg:text-[23px]">
           About Us
         </span>
         <span className="w-full md:h-1 h-[3px]  top-5 md:top-full bg-primary rounded-2xl absolute bottom-0 md:opacity-0 transition-opacity duration-300 ease-in-out opacity-100 md:group-hover:opacity-100" />
@@ -43,7 +43,7 @@ export default function NavBar() {
        onClick={changeVoicesColor}
         className="text-primary font-bold flex-col justify-start items-center inline-flex relative text-[2.0vh]"
       >
-        <span className={`md:px-4 md:py-1 ${voiceIconColor} ${voiceIcon}  bg-white  md:bg-clip-border rounded-full md:bg-red-500 md:text-white md:hover:bg-gradient-to-r from-red-500 to-purple-500 text-center font-bold`}>
+        <span className={`md:px-4 md:py-1 ${voiceIconColor} ${voiceIcon} lg:text-[23px] bg-white  md:bg-clip-border rounded-full md:bg-red-500 md:text-white md:hover:bg-gradient-to-r from-red-500 to-purple-500 text-center font-bold`}>
           Unmatched Voices
         </span>
         <span className="w-full md:h-1 h-[3px] top-5 md:top-full bg-primary ${voiceIcon} rounded-2xl absolute bottom-0 md:opacity-0 transition-opacity duration-300 ease-in-out opacity-100 md:group-hover:opacity-100" />
