644168570b1e819dfc6fc98ce9b34a7970d39337
halimAremu <halimat3000@gmail.com>
2023-11-21 07:30:57 -0600

    Add home page

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -2,16 +2,20 @@
 import React from 'react';
 
 // LOCAL IMPORTS
+import HeroCarousel from '../components/HeroCarousel.jsx';
+import StoryMission from '../components/StoryMission.jsx';
+import Card from '../components/Card.jsx';
 
-export function Home() {
+export function Home({ fullDataSet }) {
   return (
-    <div className="hero min-h-screen">
-      <div className="hero-content text-center">
-        <div className="max-w-md">
-          <h1 className="text-5xl font-bold">Welcome to Unmatched</h1>
-          <p className="py-6">Unmatched helps diverse STEM learners dealing with <span style={{color:"red"}}>Impostor Syndrome</span>  see themselves in STEM</p>
-          <button className="btn btn-primary">Discover</button>
+    <div className="bg-base-100">
+      <HeroCarousel />
+      <StoryMission />
+      <div className="max-w-[1200px] xl:mx-auto md:mx-28 sm:mx-8">
+        <div className="lg:text-4xl text-3xl font-bold md:float-left text-center text-primary px-4">
+            Explore Unmatched's Empowering Voices
         </div>
+        <Card data={fullDataSet} limit={3} />
       </div>
     </div>
   )
