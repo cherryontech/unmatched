d2909e1d7ffb96dd2f8aecda3aa12c7ba5ab4613
halimAremu <halimat3000@gmail.com>
2023-11-21 19:28:47 -0600

    removes min width

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,8 @@
 import SingleCard from './SingleCard';
 
-const Card = ({ data, limit }) => {
-  const slicedData = limit ? data.slice(0, limit) : data;
-  const details = slicedData.map(detail => {
+const Card = ({ data }) => {
+
+  const details = data.map(detail => {
     return (
       <SingleCard
         id={detail.id}
@@ -16,7 +16,7 @@ const Card = ({ data, limit }) => {
 
   return (
     <>
-      <div className="max-w-[1200px] min-w-[680px] xl:mx-auto md:mx-28 sm:mx-8">
+      <div className="max-w-[1200px] xl:mx-auto md:mx-28 sm:mx-8">
         <div className="w-full py-[2rem] md:py-[4rem] px-4">
           <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-12 place-items-center m-auto">
             {details}
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -4,18 +4,40 @@ import React from 'react';
 // LOCAL IMPORTS
 import HeroCarousel from '../components/HeroCarousel.jsx';
 import StoryMission from '../components/StoryMission.jsx';
-import Card from '../components/Card.jsx';
+import SingleCard from '../components/SingleCard.jsx';
 
 export function Home({ fullDataSet }) {
+  const limitData = []
+  for(let i = 0; i<3; i++){
+    limitData.push(fullDataSet[i])
+  };
+  const limitDataCard = limitData.map(data => {
+    return (
+      <SingleCard
+        id={data.id}
+        key={data.id}
+        image={data.image}
+        name={data.fullName}
+        body={data.occupation}
+      />
+    );
+  });
+
   return (
     <div className="bg-base-100">
       <HeroCarousel />
       <StoryMission />
-      <div className="max-w-[1200px] xl:mx-auto md:mx-28 sm:mx-8">
-        <div className="lg:text-4xl text-3xl font-bold md:float-left text-center text-primary px-4">
+      <div className="max-w-[1200px] xl:mx-auto md:mx-8 sm:mx-8">
+        <div className="lg:text-4xl text-3xl font-bold text-center text-primary px-4">
             Explore Unmatched's Empowering Voices
         </div>
-        <Card data={fullDataSet} limit={3} />
+        <div className="max-w-[1200px] xl:mx-auto md:mx-28 sm:mx-8">
+        <div className="w-full py-[2rem] md:py-[4rem] px-4">
+          <div className="grid lg:grid-cols-3 md:grid-cols-2 md:m-auto sm:grid-cols-1 gap-12 place-items-center m-auto">
+        {limitDataCard}
+        </div>
+        </div>
+        </div>
       </div>
     </div>
   )
