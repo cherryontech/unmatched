afddf431933099b46efd7bd197aeb736afe6a0dc
halimAremu <halimat3000@gmail.com>
2023-11-24 18:03:48 -0600

    edits text

--- src/components/HeroCarousel.jsx
+++ src/components/HeroCarousel.jsx
@@ -13,7 +13,7 @@ export default function HeroCarousel({scroll}) {
   return (
     <div className="carousel w-[100%] scroll-smooth">
       {/* Slide 1 */}
-      <Carousel>
+      <Carousel slideInterval={8000}>
       <div id="slide1" className="carousel-item relative  w-[100%] scroll-smooth">
         <div className="hero  h-[30rem] lg:h-[35rem] max-h-[100%]  bg-primary">
           <div className="hero-content flex-col lg:flex-row-reverse">
@@ -56,8 +56,8 @@ export default function HeroCarousel({scroll}) {
             <img src={carousel3} className="max-w-sm  mx-auto w-[30%] h-[11rem] lg:h-[20rem] shadow-2xl" />
             <div className='h-[305px] w-[80%] lg:w-[50rem] sm:h-fit md:justify-start lg:text-start  justify-center text-center '>
               <p className="lg:text-5xl py-1 text-white font-bold text-[25px] lg:text-[20px]]">Connect with STEM Voices</p>
-              <p className="py-1 text-white text-[16px] w-[97%] lg:text-[21px] lg:w-[85%]">Uncover stories of STEM leaders who’ve overcome imposter syndrome and fostered a more inclusive and empowering STEM community. </p>
-              <p className="py-1 text-white text-[16px] w-[97%] lg:text-[21px] lg:w-[85%]"> Find strength and inspiration in their experiences as you navigate your own STEM path. </p>
+              <p className="py-1 text-white text-[16px] w-[97%] lg:text-[21px] lg:w-[85%]">Uncover stories of STEM leaders who’ve overcome imposter syndrome and fostered a more inclusive STEM community. </p>
+              <p className="py-1 text-white text-[16px] w-[97%] lg:text-[21px] lg:w-[85%]"> Find strength and inspiration as you navigate your own STEM path. </p>
               <div className='mt-[2rem]'>
               <Link onClick={()=>scroll()} className="bg-secondary rounded-full px-4 lg:py-2 p-1 overflow-hidden text-white lg:text-[20px] text-[15px] font-bold hover:bg-gradient-to-r from-red-500 to-purple-500">Learn More</Link>
             </div>
