4d01991dca6ea6baca5228580e619f47ea5937f3
shyusu4 <shyusupova4@gmail.com>
2023-11-29 20:36:12 +0300

    Modify styles for slide 1 and slide 3 responsiveness

--- src/components/Slide1.jsx
+++ src/components/Slide1.jsx
@@ -7,52 +7,47 @@ import carousel1 from '../assets/images/home/carousel1.png';
 
 export default function Slide1({ scroll }) {
   return (
-    <div className="carousel-item relative">
-      <div className="hero h-[30rem] lg:h-[35rem] md:h-[30rem] sm:h-[25rem] max-h-screen w-full bg-primary">
-        <div className="hero-content flex-col lg:flex-row items-center lg:items-start">
-          {/* Text Section */}
-          <div className="flex-1 px-4 lg:px-8 text-center lg:text-left">
-            <p className="text-3xl lg:text-5xl font-bold text-white mt-4 lg:mt-0">
-              End Imposter Syndrome
+    <div className="relative flex items-center justify-center h-full bg-primary">
+      <div className="md:w-[70%] w-[90%] mx-auto flex flex-col lg:flex-row justify-center items-center xl:gap-16 lg:gap-10 gap-4 text-white">
+        <div className="flex flex-col gap-0 md:gap-5 justify-center text-center lg:text-left">
+          <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold">
+            End Imposter Syndrome
+          </h1>
+          <div className="xl:text-2xl lg:text-xl lg:w-[85%] flex flex-col gap-1 lg:gap-4">
+            <p>Every unique perspective adds value to the world of STEM.</p>
+            <p>
+              Free yourself from not fitting in and pave the way for your STEM
+              journey.
             </p>
-            <p className="mt-4 text-white text-sm sm:text-base md:text-lg lg:text-xl">
-              Every unique perspective adds value to the world of STEM.{' '}
-            </p>
-            <p className="mt-2 mb-4 text-white text-sm sm:text-base md:text-lg lg:text-xl">
-              Free yourself from not fitting in and pave the way for your
-              STEM journey.{' '}
-            </p>
-            {/* Button for large screens */}
-            <div className="mt-[2rem] hidden lg:block">
-              <Link
-                onClick={() => scroll()}
-                className="bg-secondary rounded-full px-4 lg:py-2 p-1 overflow-hidden text-white lg:text-[20px] text-[15px] font-bold hover:bg-gradient-to-r from-red-500 to-purple-500"
-              >
-                Learn More
-              </Link>
-            </div>
           </div>
-          {/* Image */}
-          <div className="flex-1 flex justify-center lg:justify-end">
-            <div className="lg:w-72 lg:h-72 md:w-52 md:h-52 sm:w-40 sm:h-40 w-40 h-40 rounded-full overflow-hidden">
-              <img
-                src={carousel1}
-                className="w-full h-full object-cover"
-                alt="Description"
-              />
-            </div>
-          </div>
-          {/* Button for small and medium screens */}
-          <div className="mt-[2rem] lg:hidden w-full text-center">
+          {/* Button for large screens */}
+          <div className="hidden lg:block">
             <Link
               onClick={() => scroll()}
-              className="bg-secondary rounded-full px-4 py-2 text-white text-[20px] font-bold hover:bg-gradient-to-r from-red-500 to-purple-500"
+              className="bg-secondary rounded-full px-4 lg:py-2 p-1 overflow-hidden lg:text-lg text-base font-bold hover:bg-gradient-to-r from-red-500 to-purple-500"
             >
               Learn More
             </Link>
           </div>
         </div>
+        {/* Image */}
+        <div className="lg:w-80 lg:h-80 md:w-56 md:h-56 w-40 h-40 mask mask-circle">
+          <img
+            src={carousel1}
+            className="w-full h-full object-cover"
+            alt="Abstract art of outlines of people filled in with colorful patterns representing individuals' uniqueness."
+          />
+        </div>
+        {/* Button for small and medium screens */}
+        <div className="lg:hidden w-full text-center">
+          <Link
+            onClick={() => scroll()}
+            className="bg-secondary rounded-full px-4 py-2 text-sm md:text-base font-bold hover:bg-gradient-to-r from-red-500 to-purple-500"
+          >
+            Learn More
+          </Link>
+        </div>
       </div>
     </div>
-  )
+  );
 }
--- src/components/Slide3.jsx
+++ src/components/Slide3.jsx
@@ -7,37 +7,50 @@ import carousel3 from '../assets/images/home/carousel3.png';
 
 export default function Slide3({ scroll }) {
   return (
-    <div className="carousel-item relative w-full scroll-smooth">
-      <div className="hero  h-[30rem] lg:h-[35rem] max-h-[100%] bg-primary">
-        <div className="hero-content flex-col lg:flex-row-reverse">
+    <div className="relative flex items-center justify-center h-full bg-primary">
+      <div className="md:w-[70%] w-[90%] mx-auto flex flex-col lg:flex-row justify-center items-center xl:gap-16 lg:gap-10 gap-4 text-white">
+        <div className="flex flex-col gap-0 md:gap-5 justify-center text-center lg:text-left">
+          <h1 className="text-2xl md:text-4xl lg:text-5xl font-bold">
+            Connect with STEM Voices
+          </h1>
+          <div className="xl:text-2xl lg:text-xl lg:w-[85%] flex flex-col items-center justify-center gap-4">
+            <p>
+              Uncover stories of STEM leaders who’ve overcome imposter syndrome
+              and fostered a more inclusive and empowering STEM community.
+            </p>
+            <p>
+              Find strength and inspiration in their experiences as you navigate
+              your own STEM path.
+            </p>
+          </div>
+          {/* Button for large screens */}
+          <div className="hidden lg:block">
+            <Link
+              onClick={() => scroll()}
+              className="bg-secondary rounded-full px-4 lg:py-2 p-1 overflow-hidden lg:text-lg text-base font-bold hover:bg-gradient-to-r from-red-500 to-purple-500"
+            >
+              Learn More
+            </Link>
+          </div>
+        </div>
+        {/* Image */}
+        <div className="xl:w-[80%] lg:w-full lg:h-72 w-40 h-40">
           <img
             src={carousel3}
-            className="max-w-sm  mx-auto w-[30%] h-[5rem] lg:h-[20rem] shadow-2xl"
+            className="w-full h-full object-cover rounded-md"
+            alt=""
           />
-          <div className="h-[305px] w-[80%] lg:w-[50rem] sm:h-fit md:justify-start lg:text-start  justify-center text-center ">
-            <p className="lg:text-5xl py-1 text-white font-bold text-[25px] lg:text-[20px]]">
-              Connect with STEM Voices
-            </p>
-            <p className="lg:py-1 py-0 text-white text-[16px] w-[97%] lg:text-[21px] lg:w-[85%]">
-              Uncover stories of STEM leaders who’ve overcome imposter
-              syndrome and fostered a more inclusive STEM community.{' '}
-            </p>
-            <p className="lg:py-1 py-0 text-white text-[16px] w-[97%] lg:text-[21px] lg:w-[85%]">
-              {' '}
-              Find strength and inspiration as you navigate your own STEM
-              path.{' '}
-            </p>
-            <div className="mt-[2rem]">
-              <Link
-                onClick={() => scroll()}
-                className="bg-secondary rounded-full px-4 lg:py-2 p-1 overflow-hidden text-white lg:text-[20px] text-[15px] font-bold hover:bg-gradient-to-r from-red-500 to-purple-500"
-              >
-                Learn More
-              </Link>
-            </div>
-          </div>
+        </div>
+        {/* Button for small and medium screens */}
+        <div className="lg:hidden w-full text-center">
+          <Link
+            onClick={() => scroll()}
+            className="text-sm md:text-base bg-secondary rounded-full px-3 py-1 md:px-4 md:py-2 font-bold hover:bg-gradient-to-r from-red-500 to-purple-500"
+          >
+            Learn More
+          </Link>
         </div>
       </div>
     </div>
-  )
+  );
 }
