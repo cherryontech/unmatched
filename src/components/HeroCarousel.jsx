f1c7bcd935a9c6e629d0f72e1ff98ae3d0897390
shyusu4 <shyusupova4@gmail.com>
2023-11-29 12:06:36 +0300

    Modify carousel styles

--- src/components/HeroCarousel.jsx
+++ src/components/HeroCarousel.jsx
@@ -1,6 +1,5 @@
 // LIBRARY IMPORTS
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 import { Carousel } from 'flowbite-react';
 import { ChevronRightIcon, ChevronLeftIcon } from '@heroicons/react/24/outline';
 
@@ -9,28 +8,58 @@ import Slide1 from './Slide1.jsx';
 import Slide2 from './Slide2.jsx';
 import Slide3 from './Slide3.jsx';
 
+const customTheme = {
+  scrollContainer: {
+    base: 'relative flex h-full scroll-smooth',
+    snap: 'snap-x',
+  },
+  control: {
+    base: '',
+    icon: '',
+  },
+  indicators: {
+    active: {
+      off: 'hover:bg-primary bg-stone-300',
+      on: 'bg-primary',
+    },
+    base: 'h-3 w-3 rounded-full',
+    wrapper: 'absolute -bottom-13 py-4 flex justify-center gap-3 w-full',
+  },
+};
+
 export default function HeroCarousel({ scroll }) {
+  const [activeControl, setActiveControl] = useState(0);
+  const handleSlideChange = index => {
+    setActiveControl(index);
+  };
+
   return (
-    <div className="carousel w-full scroll-smooth">
+    <>
       <Carousel
+        theme={customTheme}
         slideInterval={8000}
+        onSlideChange={handleSlideChange}
         leftControl={
-          <ChevronLeftIcon
-            className="w-10 text-white"
-            style={{ strokeWidth: 3 }}
-          />
+          activeControl !== 0 && (
+            <ChevronLeftIcon
+              className="w-10 text-white"
+              style={{ strokeWidth: 3 }}
+            />
+          )
         }
         rightControl={
-          <ChevronRightIcon
-            className="w-10 text-white"
-            style={{ strokeWidth: 3 }}
-          />
+          activeControl !== 2 && (
+            <ChevronRightIcon
+              className="w-10 text-white"
+              style={{ strokeWidth: 3 }}
+            />
+          )
         }
       >
-        <Slide1 scroll = {scroll} />
-        <Slide2 scroll = {scroll} />
-        <Slide3 scroll = {scroll} />
+        <Slide1 scroll={scroll} />
+        <Slide2 scroll={scroll} />
+        <Slide3 scroll={scroll} />
       </Carousel>
-    </div>
+    </>
   );
 }
